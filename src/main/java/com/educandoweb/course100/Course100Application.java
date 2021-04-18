84fbb1242d3f6cbcc2ca107c79aa11dca2354b7c
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-04-18 14:44:42 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course100/Course100Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course100Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/course100/Course100ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course100ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
